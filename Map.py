f853ed1918de3aabaa55761eefc7a67b4e2a5884
Jake Gillenwater <JakeGill70@gmail.com>
2021-03-24 03:39:40 -0400

    Import map data

---
+++ Map.py
@@ -0,0 +1,46 @@
+import sys
+
+
+class Map():
+    def __init__(self):
+        super().__init__()
+        self.mapData = {}
+        self.continents = {}
+
+    def readMapData(self, filePath):
+        currentContinent = ""
+        f = open(filePath, "r")
+        for line in f:
+            # Ignore comment lines
+            if(line[0] == "#"):
+                continue
+
+            # Process continent lines
+            if(line.strip().startswith("continent")):
+                try:
+                    currentContinent = line.split("=")[1].strip()
+                    self.continents[currentContinent] = []
+                    continue
+                except IndexError:
+                    print(f"Malform line in mapdata: '{line}'", file=sys.stderr)
+                    continue
+
+            # Process data lines
+            try:
+                data = line.split(":")
+                index = int(data[0].strip())
+                connections = [int(x.strip()) for x in data[1].split(",")]
+                self.mapData[index] = connections
+                self.continents[currentContinent].append(index)
+            except IndexError:
+                print(f"Malform line in mapdata: '{line}'", file=sys.stderr)
+                continue
+        f.close()
+
+        # Verify all connections are bi-directional
+        for index, connections in self.mapData.items():
+            for i in connections:
+                if(index not in self.mapData[i]):
+                    print(
+                        f"Uni-directional connection found. {index} is connected to {i}, but {i} is not connected to {index}",
+                        file=sys.stderr)
--- RiskDriver.py
+++ RiskDriver.py
@@ -1,1 +1,6 @@
 
+from Map import Map
+
+map = Map()
+map.readMapData("MapData.txt")
+print(map.continents)
