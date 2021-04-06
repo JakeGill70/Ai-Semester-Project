8bfdd536a205df194bb2be8428bca31e65fb728a
Jake Gillenwater <JakeGill70@gmail.com>
2021-04-06 15:31:33 -0400

    Territory toString() implemented

--- Territory.py
+++ Territory.py
@@ -6,3 +6,6 @@ class Territory():
         self.position = position
         self.owner = ""
         self.army = 0
+
+    def __str__(self):
+        return f"#{self.index}:{self.owner}:{self.army}"
