1e5e023524a8b5b2a1859712679a3560aba3bca7
Jake Gillenwater <JakeGill70@gmail.com>
2021-04-06 19:48:33 -0400

    Agent attack selection implemented

--- Agent.py
+++ Agent.py
@@ -1,3 +1,9 @@
+from collections import namedtuple
+
+
+AttackSelection = namedtuple('AttackSelection', 'attackIndex defendIndex estimateResult')
+
+
 class AgentCharacteristic:
     def __init__(self, value, description):
         self.value = value
@@ -18,6 +24,15 @@ class Agent:
                 "Border Adjacent": AgentCharacteristic(13, "Placing a unit in a territory that borders a country in a different continent"),
                 "Connection Bias": AgentCharacteristic(1, "Placing a unit on a territory with connections to multiple other countries, +value per connection"),
                 "Placement Bias Multiplier": AgentCharacteristic(0.5, "Placing a unit where there already are other units, *value per army")
+            },
+            "Attack": {
+                "Anywhere": AgentCharacteristic(3, "Attacking anywhere"),
+                "Ally Adjacent": AgentCharacteristic(5, "Attacking a territory connected to another territory controlled by the attacking player"),
+                "Border Adjacent": AgentCharacteristic(8, "Attacking a territory on the border of a different continent"),
+                "Destroy Bias": AgentCharacteristic(1, "Estimated amount of defending units destroyed, +1 value per unit"),
+                "Remain Bias": AgentCharacteristic(-1, "Estimated amount of attacking units destroyed, -1 value per unit"),
+                "Safe Threshold": AgentCharacteristic(0.95, "Minimal amount of estimated chance of a successful attack to consider an attack safe, below this amount is considered risky"),
+                "Minimal Success Chance": AgentCharacteristic(0.5, "Minimal amount of estimated chance of successful attack necessary for an attack to be considered viable")
             },
             "Preference": {
                 "Larger": AgentCharacteristic(1, "Preference to attack larger players"),
@@ -105,6 +120,69 @@ class Agent:
             #print(f"Index: {territoryData.index}, Score: {score}, BestIndex: {bestIndex}")
 
         return bestIndex
+
+    def pickTerritoryForAttack(self, map, atkSys):
+        controlledTerritories = map.getTerritoriesByPlayer(self.name)
+        controlledTerritoriesThatCanAttack = [x for x in controlledTerritories if x.army > 1]
+
+        score = -1
+        bestScore = -1
+        bestAttackingTerritory = None
+        bestDefendingTerritory = None
+        bestAttackEstimate = None
+        for territory in controlledTerritoriesThatCanAttack:
+            enemyConnections = [map.territories[index]
+                                for index in territory.connections if map.territories[index].owner != self.name]
+            for enemyTerritory in enemyConnections:
+                # Get attack estimate
+                attackEstimate = atkSys.getAttackEstimate(territory.army, enemyTerritory.army)
+
+                # Determine if the attack is viable
+                if(attackEstimate.attackSuccessChance < self.characteristics["Attack"]["Minimal Success Chance"].value):
+                    # Don't bother calculating the score from this point onward
+                    # rm print(f"Attacking ({enemyTerritory}) from ({territory}) is a non-viable strategy with only a {attackEstimate.attackSuccessChance * 100}% chance of success")
+                    continue
+
+                score = 0
+                score += self.characteristics["Attack"]["Anywhere"].value
+                score += self.characteristics["Attack"]["Ally Adjacent"].value if self.getTerritoryDataEnemyAdjacent(
+                    territory.index, map) else 0
+                score += self.characteristics["Attack"]["Border Adjacent"].value if self.getTerritoryDataBorderAdjacent(
+                    enemyTerritory.index, map) else 0
+
+                score += self.characteristics["Attack"]["Remain Bias"].value * \
+                    (territory.army - attackEstimate.attackers)
+                score += self.characteristics["Attack"]["Destroy Bias"].value * \
+                    (enemyTerritory.army - attackEstimate.defenders)
+
+                score += self.characteristics["Preference"]["Risky"].value if \
+                    attackEstimate.attackSuccessChance < self.characteristics["Attack"]["Safe Threshold"].value else 0
+                score += self.characteristics["Preference"]["Safe"].value if  \
+                    attackEstimate.attackSuccessChance >= self.characteristics["Attack"]["Safe Threshold"].value else 0
+
+                # If there is a best value to compare to
+                if(bestScore != -1):
+                    currEnemySize = len(map.getTerritoriesByPlayer(enemyTerritory.owner))
+                    bestEnemySize = len(map.getTerritoriesByPlayer(bestDefendingTerritory.owner))
+                    if(currEnemySize > bestEnemySize):
+                        score += self.characteristics["Preference"]["Larger"].value
+                    if(currEnemySize < bestEnemySize):
+                        score += self.characteristics["Preference"]["Smaller"].value
+
+                if(score > bestScore):
+                    bestScore = score
+                    bestAttackingTerritory = territory
+                    bestDefendingTerritory = enemyTerritory
+                    bestAttackEstimate = attackEstimate
+
+                # rm print(f"Attacking ({enemyTerritory}) from ({territory}) scored {score}, and has a {attackEstimate.attackSuccessChance * 100}% chance of success. Best Score: {bestScore}")
+
+        # Return 'None' if there are no viable attack options
+        if(bestScore == -1):
+            return None
+
+        # Return the best option found
+        return AttackSelection(bestAttackingTerritory.index, bestDefendingTerritory.index, bestAttackEstimate)
 
     def placeUnit(self, map):
         controlledTerritoryIndices = map.getTerritoriesByPlayer(self.name)
--- RiskDriver.py
+++ RiskDriver.py
@@ -3,6 +3,7 @@ import random
 from Game import Game
 from Territory import Territory
 from Agent import Agent
+from AttackSystem import AttackSystem
 
 
 def setupGameBoard(agentList, initialUnits, map):
@@ -18,9 +19,13 @@ def setupGameBoard(agentList, initialUnits, map):
 game = Game()
 map = Map()
 map.readMapData("MapData.txt")
+atkSys = AttackSystem()
 
 agents = [Agent("Jake"), Agent("Xander"), Agent("Sabrina"), Agent("Rusty")]
 
 setupGameBoard(agents, 30, map)
+
+pickTerritoryResult = agents[0].pickTerritoryForAttack(map, atkSys)
+print(pickTerritoryResult)
 
 game.showWindow(map)
