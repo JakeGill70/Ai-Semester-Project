5095350f76c4fdd03f3cb756cb40a62b29d62cac
Jake Gillenwater <JakeGill70@gmail.com>
2021-05-20 15:00:58 -0400

    Implemented a basic tournament structure

--- Tournament.py
+++ Tournament.py
@@ -13,6 +13,29 @@ class TournamentEntry:
 class Tournament:
     def __init__(self):
         self.entries = []
+        self.competitorIndex = 0
 
     def addToTournament(self, agent):
         self.entries.append(TournamentEntry(agent))
+
+    def getCompetitors(self, groupSize=2):
+        competitors = self.entries[self.competitorIndex:(self.competitorIndex+groupSize)]
+        self.competitorIndex += groupSize
+        self.competitorIndex = self.competitorIndex % len(self.entries)
+        return competitors
+
+    def getTournamentTier(self, groupSize=2):
+        matchUps = []
+        for i in range(0, len(self.entries), groupSize):
+            matchUp = self.entries[i:i+groupSize]
+            matchUps.append(matchUp)
+        return matchUps
+
+    def sortTournament(self):
+        # Sort first by losses (low to high),
+        # then by score (low to high)
+        self.entries.sort(key=lambda x: (x.losses, x.score))
+
+    def removeByMaxLosses(self, maxLosses=1):
+        self.entries = [x for x in self.entries if x.losses < maxLosses]
+
