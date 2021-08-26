50a3b0b5d9a26ae1819c65fd9a0f090ee307e98c
Jake Gillenwater <JakeGill70@gmail.com>
2021-08-25 23:16:08 -0400

    Simple bash script for running the application on a server

---
+++ serverRunHeadless.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the RiskDriver.py script using python3.
+# Detach it from the terminal using nohup.
+# Forward stdout to null because it is not necessary to log the output.
+# Forward stderr to stdout, which will itself be forwarded to nohup.out.
+#	because we do want to log error messages in the event of a crash.
+# Lastly, place a & at the end so this task runs in the background.
+
+nohup python3 RiskDriver.py > /dev/null 2>&1 &
