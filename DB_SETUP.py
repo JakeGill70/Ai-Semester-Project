a899e5acc9d63345f4d951a6b47dc5e6be603d5a
Jake Gillenwater <JakeGill70@gmail.com>
2021-12-10 13:05:21 -0500

    Added unqiue constraint to map/player hash in cache DB

--- DB_SETUP.py
+++ DB_SETUP.py
@@ -4,8 +4,12 @@ con = sqlite3.connect("RiskCache.db")
 cur = con.cursor()
 
 # Create Table
-cur.execute("""CREATE TABLE MapCache 
-                (mapHash char(32), playerHash char(32), score DOUBLE);""")
+cur.execute("""CREATE TABLE MapCache (
+                    mapHash char(32), 
+                    playerHash char(32), 
+                    score DOUBLE,
+                    UNIQUE(mapHash, playerHash)
+                );""")
 
 con.commit()
 
