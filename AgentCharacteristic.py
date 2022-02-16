b8b5b74c13121625f4d3aa5b0ddc3382308bc1d6
Jake Gillenwater <JakeGill70@gmail.com>
2022-02-15 19:39:40 -0500

    Refactored AgentCharacteristic to be more pythonic

--- AgentCharacteristic.py
+++ AgentCharacteristic.py
@@ -19,38 +19,26 @@ class AgentCharacteristic:
         return self.value
 
     def __deepcopy__(self):
-        cpy = AgentCharacteristic()
-        cpy.value = self.value
-        cpy.description = self.description
-        cpy.adjustmentAmt = self.adjustmentAmt
+        cpy = AgentCharacteristic(self.value, self.description, self.adjustmentAmt, self.lowerLimit, self.upperLimit)
         return cpy
 
     def toJSON(self):
         return f"{{\"value\" : {self.value}, \"adjustmentAmount\" : {self.adjustmentAmt}, \"lowerLimit\" : {self.lowerLimit}, \"upperLimit\" : {self.upperLimit}, \"description\" : \"{self.description}\"}}"
 
-    def adjust(self):
-        self.adjust(self.adjustmentAmt)
-
-    def adjust(self, amt):
+    def adjust(self, amt=None):
+        amt = self.adjustmentAmt if amt is None else amt
         self.value += amt
         if (self.value > self.upperLimit):
             self.value = self.upperLimit
         elif (self.value < self.lowerLimit):
             self.value = self.lowerLimit
 
-    def adjust_negative(self):
-        self.adjust(-self.adjustmentAmt)
-
-    def adjust_negative(self, amt):
+    def adjust_negative(self, amt=None):
+        amt = self.adjustmentAmt if amt is None else amt
         self.adjust(-amt)
 
-    def adjust_random(self):
-        if (bool(random.getrandbits(1))):
-            self.adjust()
-        else:
-            self.adjust_negative()
-
     def adjust_random(self, amt):
+        amt = self.adjustmentAmt if amt is None else amt
         if (bool(random.getrandbits(1))):
             self.adjust(amt)
         else:
